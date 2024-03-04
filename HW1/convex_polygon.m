67b443530cd9dcc8b54089d89420844bfd035269
KB-83 <kajal.baghestani@gmail.com>
2024-03-04 21:47:36 +0330

    bug fixed for finding left and right points

--- HW1/convex_polygon.m
+++ HW1/convex_polygon.m
@@ -1,6 +1,5 @@
-n = 10;
-X = [3,4,1,2,5];
-Y = [5,1,4,2,3];
+X = [4,0,-1,13,7,-5];
+Y = [10,0,11,4,-3,7];
 max_y = Y(1);
 max_index = 1;
 min_y = Y(1);
@@ -16,7 +15,36 @@ for i = 2:length(Y)
     end
 end
 hold on;
+% part a
 plot(X, Y, 'bo');
+
+% calculate the slope of the line connecting the maximum and minimum points
+slope = (max_y - min_y) / (X(max_index) - X(min_index));
+
+% lets sort the left side and right side points by their positions relative to the line
+left_indices = [];
+right_indices = [];
+for i = 1:length(X)
+    if ((X(i) - X(min_index)) * (Y(max_index) - Y(min_index)) - (Y(i) - Y(min_index)) * (X(max_index) - X(min_index)) > 0)
+        right_indices = [right_indices, i];
+    else
+        left_indices = [left_indices, i];
+    end
+end
+
+% plot the points
 plot(X(max_index), Y(max_index), 'rx', 'MarkerSize', 10); 
 plot(X(min_index), Y(min_index), 'gx', 'MarkerSize', 10);
 plot([X(max_index), X(min_index)], [Y(max_index), Y(min_index)], 'k-');
+
+% plot the sorted polygon
+left_points = [X(left_indices); Y(left_indices)]';
+right_points = [X(right_indices); Y(right_indices)]';
+sorted_left = sortrows(left_points, 2); % sort by y-value
+sorted_right = sortrows(right_points, 2); 
+sorted_X = [sorted_left(:, 1); sorted_right(end:-1:1, 1)];
+sorted_Y = [sorted_left(:, 2); sorted_right(end:-1:1, 2)];
+sorted_X = [sorted_X; sorted_X(1)];
+sorted_Y = [sorted_Y; sorted_Y(1)];
+plot(sorted_X, sorted_Y, 'r-');
+hold off;
